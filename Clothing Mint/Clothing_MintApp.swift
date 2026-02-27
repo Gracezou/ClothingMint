82e185b4796c69f886e8530e232edae042ff3c54
zhaoshian <295057329@qq.com>
2026-02-27 16:22:54 +0800

    Initial Commit

---
+++ Clothing Mint/Clothing_MintApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clothing_MintApp.swift
+//  Clothing Mint
+//
+//  Created by zhaoshian on 2026/2/27.
+//
+
+import SwiftUI
+
+@main
+struct Clothing_MintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clothing Mint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clothing Mint
+//
+//  Created by zhaoshian on 2026/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
